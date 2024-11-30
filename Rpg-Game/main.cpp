e6d5298b58df070b5e75b5c39c348d54c0780a4f
MaxCode012 <seremet.maxim94@gmail.com>
2024-11-30 17:17:23 +0200

    Added a player with movement and with the ability to shoot bullets
    -Added skeleton spritesheet
    -Added the ability to shoot bullets

--- Rpg-Game/main.cpp
+++ Rpg-Game/main.cpp
@@ -1,15 +1,56 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+#include <math.h>
+
+sf::Vector2f NormalizeVector(sf::Vector2f vector) {
+    float m = std::sqrt(vector.x* vector.x + vector.y + vector.y);
+    
+    sf::Vector2f normalizedVector;
+
+    normalizedVector.x = vector.x / m;
+    normalizedVector.y = vector.y / m;
+
+    return normalizedVector;
+}
 
 int main()
 {
     //---------------------------------- INITIALIZE -----------------------
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode(800, 600), "RPG Game", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(1200, 720), "RPG Game", sf::Style::Default, settings);
     //---------------------------------- INITIALIZE -----------------------
+    
+    std::vector<sf::RectangleShape> bullets;
+    float bulletSpeed = 0.5f;
 
     //---------------------------------- LOAD -----------------------
+
+    //---------------------------------- Enemy -----------------------
+
+    sf::Texture enemyTexture;
+    sf::Sprite enemySprite;
+
+    if (enemyTexture.loadFromFile("Assets/Skeleton/Textures/spritesheet.png")) {
+            std::cout << "Success";
+            enemySprite.setTexture(enemyTexture);
+            enemySprite.setPosition(sf::Vector2f(400, 100));
+
+            int xIndex = 8;
+            int yIndex = 3;
+
+            enemySprite.setTextureRect(sf::IntRect(xIndex * 64, yIndex * 64, 64, 64));
+            enemySprite.scale(sf::Vector2f(3, 3));
+    }
+    
+    else {
+        std::cout << "Fail";
+    }
+    //---------------------------------- Enemy -----------------------
+
+    //---------------------------------- Player -----------------------
+
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
 
@@ -27,6 +68,7 @@ int main()
     else {
         std::cout << "Fail";
     }
+    //---------------------------------- Player -----------------------
 
     //---------------------------------- LOAD -----------------------
 
@@ -39,26 +81,52 @@ int main()
             if (event.type == sf::Event::Closed())
                 window.close();
         }
+
+        //bullet.setPosition(bullet.getPosition() + bulletDir * bulletSpeed);
+
         sf::Vector2f position = playerSprite.getPosition();
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
             playerSprite.setPosition(position + sf::Vector2f(1, 0));
-        
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) 
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
             playerSprite.setPosition(position + sf::Vector2f(-1, 0));
-        
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) 
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
             playerSprite.setPosition(position + sf::Vector2f(0, -1));
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
             playerSprite.setPosition(position + sf::Vector2f(0, 1));
-        
 
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) 
+        {
+            sf::RectangleShape newBullet(sf::Vector2f(50, 25));
+            bullets.push_back(newBullet);
+
+            int i = bullets.size() - 1;
+            bullets[i].setPosition(playerSprite.getPosition());
+        }
+
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            sf::Vector2f bulletDir = enemySprite.getPosition() - bullets[i].getPosition();
+            bulletDir = NormalizeVector(bulletDir);
+            bullets[i].setPosition(bullets[i].getPosition() + bulletDir * bulletSpeed);
+    
+        }
+    
         //---------------------------------- UPDATE -----------------------
 
         window.clear(sf::Color::Black);
 
         window.draw(playerSprite);
+        window.draw(enemySprite);
+        
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            window.draw(bullets[i]);
+        }
+
         window.display();
     }
 
